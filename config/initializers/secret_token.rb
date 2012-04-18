590016784b900194b57107cae6c219957689d4b4
Guilherme Silveira <guilherme.silveira@caelum.com.br>
2012-04-18 14:18:14 -0300

    first attempt

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JsEvalNaVeia::Application.config.secret_token = '3cc1f90afaa1c0b91676285a8b628aa5f88684f6073cb1705ff9322f450368890bbba11e8950a03d0ab7ac5a29c3178dcf2b0740e4015c76bce558ecb10bb508'
